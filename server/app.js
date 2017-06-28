606fdbc4bcde507d1859df478c8df19a3a053d96
Alex Seiden <alex.seiden@gmail.com>
2017-06-28 10:45:56 -0700

    (refactor) comment out twitter code in app.js

--- server/app.js
+++ server/app.js
@@ -4,50 +4,50 @@ const path = require('path');
 const middleware = require('./middleware');
 const routes = require('./routes');
 const io = require('socket.io');
-const Twitter = require('twitter');
-const Twit = require('twit');
+// const Twitter = require('twitter');
+// const Twit = require('twit');
 const app = express();
 
-try {
-  const creds = require('../config/configVars');
-} catch (e) {
-}
+// try {
+//   const creds = require('../config/configVars');
+// } catch (e) {
+// }
 
-let twitterKey;
-let twitterSecret;
-let twitterAccessToken;
-let twitterAccessTokenSecret;
-if (process.env.TWITTER_KEY) {
-  twitterKey = process.env.TWITTER_KEY;
-} else {
-  twitterKey = creds.twitterKey;
-}
+// let twitterKey;
+// let twitterSecret;
+// let twitterAccessToken;
+// let twitterAccessTokenSecret;
+// if (process.env.TWITTER_KEY) {
+//   twitterKey = process.env.TWITTER_KEY;
+// } else {
+//   twitterKey = creds.twitterKey;
+// }
 
-if (process.env.TWITTER_SECRET) {
-  twitterSecret = process.env.TWITTER_SECRET;
-} else {
-  twitterSecret = creds.twitterSecret;
-}
+// if (process.env.TWITTER_SECRET) {
+//   twitterSecret = process.env.TWITTER_SECRET;
+// } else {
+//   twitterSecret = creds.twitterSecret;
+// }
 
-if (process.env.TWITTER_ACCESS_TOKEN) {
-  twitterAccessToken = process.env.TWITTER_ACCESS_TOKEN;
-} else {
-  twitterAccessToken = creds.twitterAccessToken;
-}
+// if (process.env.TWITTER_ACCESS_TOKEN) {
+//   twitterAccessToken = process.env.TWITTER_ACCESS_TOKEN;
+// } else {
+//   twitterAccessToken = creds.twitterAccessToken;
+// }
 
-if(process.env.TWITTER_ACCESS_TOKEN_SECRET) {
-  twitterAccessTokenSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
-} else {
-  twitterAccessTokenSecret = creds.twitterAccessTokenSecret;
-}
+// if(process.env.TWITTER_ACCESS_TOKEN_SECRET) {
+//   twitterAccessTokenSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
+// } else {
+//   twitterAccessTokenSecret = creds.twitterAccessTokenSecret;
+// }
 
-var T = new Twit({
-    consumer_key: twitterKey,
-    consumer_secret: twitterSecret,
-    access_token: twitterAccessToken,
-    access_token_secret: twitterAccessTokenSecret,
-    timeout_ms: 60*1000
-});
+// var T = new Twit({
+//     consumer_key: twitterKey,
+//     consumer_secret: twitterSecret,
+//     access_token: twitterAccessToken,
+//     access_token_secret: twitterAccessTokenSecret,
+//     timeout_ms: 60*1000
+// });
 
 app.use(middleware.morgan('dev'));
 app.use(middleware.cookieParser());
@@ -74,15 +74,15 @@ app.use('/api/maps', routes.maps);
 app.use('/api/feed', routes.feed);
 
 
-app.get('/getTweet', function (req, res) {
-  T.get('search/tweets', { q: req.query.searchTerm, count: 100 }, function(err, data, response) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(data.statuses);
-    }
-  })
-})
+// app.get('/getTweet', function (req, res) {
+//   T.get('search/tweets', { q: req.query.searchTerm, count: 100 }, function(err, data, response) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.send(data.statuses);
+//     }
+//   });
+// });
 
 
 module.exports = app;
