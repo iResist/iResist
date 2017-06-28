f7a4883143e208d908c77cb3f5458e717849810d
Alex Seiden <alex.seiden@gmail.com>
2017-06-28 10:58:19 -0700

    (bug) export route

--- server/routes/twitter.js
+++ server/routes/twitter.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers').Twitter;
 
-app.get('/getTweet', controllers.getTweets);
+router.get('/', controllers.getTweets);
+
+module.exports = router;
